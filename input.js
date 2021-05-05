2c3af55461074c36171fba7aee418c1ef247ed36
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-05-05 17:50:57 +0000

    WSAD movement

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,6 +13,14 @@ const setupInput = function() {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
 };
 
