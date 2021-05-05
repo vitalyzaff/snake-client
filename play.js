b4f031279ff99c458e069a1867365b262be3caec
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-05-05 16:48:50 +0000

    Module export form client.js

--- play.js
+++ play.js
@@ -1,20 +1,5 @@
-const net = require("net");
-
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.222.131" ,
-    port: 50542
-  });
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    conn.on('data', (data) => {
-      console.log(data);
-    });
-  });
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
